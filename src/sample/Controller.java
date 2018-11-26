12875d97eb6a40686faf5a0907ea5718090ec1f7
imaticevic1 <imaticevic1@etf.unsa.ba>
2018-11-26 22:26:48 +0100

    dodao ve sto bi trebalo, mali problem sa boxovima

---
+++ src/sample/Controller.java
@@ -0,0 +1,231 @@
+package sample;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Controller {
+
+    public TextField poljeBrojIndeksa;
+    public TextField jmbgPolje;
+    public TextField AdresaPolje;
+    public TextField emailPolje;
+    public Button dugmePotvrdi;
+    public TextField telefonpolje;
+    private boolean imeValidno;
+    private boolean prezimeValidno;
+    private boolean brIndeksaValidan;
+    private boolean datumValidan = false;
+    private boolean jmbgValidan = false;
+    private boolean telefonValidan = true;
+    public TextField poljePrezime;
+    public DatePicker izborDatuma;
+    public TextField poljeIme;
+    private String datumRodjenja = null;
+    private String control = null;
+    private String jmbg = null;
+    private ArrayList<String> nastavci = new ArrayList<String>();
+    {
+        nastavci.add("hotmail.com");
+        nastavci.add("gmail.com");
+        nastavci.add("yahoo.com");
+        nastavci.add("etf.unsa.ba");
+    }
+    private boolean isImePrezimeValidno(String n){
+        for(int i = 0; i < n.length(); i++){
+            if(!Character.isLetter(n.charAt(i)) || i > 19){
+                return false;
+            }
+        }
+        return !n.trim().isEmpty();
+    }
+    private boolean isNumber(String s, int granica) {
+        for(int i = 0; i < s.length(); i++){
+            if(!Character.isDigit(s.charAt(i)) || i > granica){
+                return false;
+            }
+        }
+        return !s.trim().isEmpty();
+    }
+    private boolean isValidanJMBG(String s) {
+        for(int i = 0; i < s.length(); i++){
+            if(!Character.isDigit(s.charAt(i))){
+                return false;
+            }
+            if(control != null && s.length() == 13){
+                if(!control.equals(s.substring(0,7)))
+                    return false;
+            }
+        }
+        jmbg = s;
+        return true;
+    }
+
+    public void dateHandler(ActionEvent actionEvent) {
+        LocalDate d = izborDatuma.getValue();
+        datumRodjenja = d.toString().substring(8,10) + "." + d.toString().substring(5,7) + "." + d.toString().substring(0,4);
+        if(Integer.parseInt(d.toString().substring(0, 4)) > 2017) {
+            izborDatuma.getStyleClass().removeAll("poljeIspravno");
+            izborDatuma.getStyleClass().add("poljeNeispravno");
+            datumValidan = false;
+        }
+        else {
+            izborDatuma.getStyleClass().removeAll("poljeNeispravno");
+            izborDatuma.getStyleClass().add("poljeIspravno");
+            datumValidan = true;
+        }
+        String s2 = d.toString().substring(8,10) + d.toString().substring(5,7) + d.toString().substring(1,4);
+        if(control == null)
+            control = s2;
+        if(control != s2) {
+            control = s2;
+            if(!control.equals(jmbg.substring(0,7)) && jmbgValidan && jmbg.length() == 13){
+                jmbgPolje.getStyleClass().removeAll("poljeIspravno");
+                jmbgPolje.getStyleClass().add("poljeNeispravno");
+                jmbgValidan = false;
+            }
+        }
+    }
+
+    @FXML
+    public void initialize(){
+        imeValidno = false;
+        poljeIme.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(isImePrezimeValidno(newValue)){
+                    poljeIme.getStyleClass().removeAll("poljeNeispravno");
+                    poljeIme.getStyleClass().add("poljeIspravno");
+                    imeValidno = true;
+                }
+                if(!isImePrezimeValidno(newValue)){
+                    poljeIme.getStyleClass().removeAll("poljeNeispravno","poljeIspravno");
+                    poljeIme.getStyleClass().add("poljeNeispravno");
+                    imeValidno = false;
+                }
+            }
+        });
+        prezimeValidno = false;
+        poljePrezime.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(isImePrezimeValidno(newValue)){
+                    poljePrezime.getStyleClass().removeAll("poljeNeispravno");
+                    poljePrezime.getStyleClass().add("poljeIspravno");
+                    prezimeValidno = true;
+                }
+                if(!isImePrezimeValidno(newValue)){
+                    poljePrezime.getStyleClass().removeAll("poljeNeispravno","poljeIspravno");
+                    poljePrezime.getStyleClass().add("poljeNeispravno");
+                    prezimeValidno = false;
+                }
+            }
+        });
+        brIndeksaValidan = false;
+        poljeBrojIndeksa.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(isNumber(newValue ,7)){
+                    poljeBrojIndeksa.getStyleClass().removeAll("poljeNeispravno");
+                    poljeBrojIndeksa.getStyleClass().add("poljeIspravno");
+                    brIndeksaValidan = true;
+                }
+                if(!isNumber(newValue, 7)){
+                    poljeBrojIndeksa.getStyleClass().removeAll("poljeNeispravno","poljeIspravno");
+                    poljeBrojIndeksa.getStyleClass().add("poljeNeispravno");
+                    brIndeksaValidan = false;
+                }
+            }
+
+        });
+        jmbgValidan = false;
+        jmbg = jmbgPolje.getText();
+        jmbgPolje.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(!isValidanJMBG(newValue) || newValue.length() >= 13){
+                    jmbgPolje.getStyleClass().removeAll("poljeIspravno");
+                    jmbgPolje.getStyleClass().add("poljeNeispravno");
+                    jmbgValidan = false;
+                }
+                if(isValidanJMBG(newValue) && newValue.length() == 13){
+                    jmbgPolje.getStyleClass().removeAll("poljeNeispravno");
+                    jmbgPolje.getStyleClass().add("poljeIspravno");
+                    jmbgValidan = true;
+                }
+                if(isValidanJMBG(newValue) && newValue.length() < 13)
+                    jmbgPolje.getStyleClass().removeAll("poljeNeispravno", "poljeIspravno");
+            }
+        });
+        emailPolje.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                for(String s : nastavci){
+                    if(newValue.endsWith(s)){
+                        emailPolje.getStyleClass().add("poljeIspravno");
+                        break;
+                    }
+                    else emailPolje.getStyleClass().removeAll("poljeIspravno");
+                }
+            }
+        });
+        telefonpolje.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(!isNumber(newValue, 10) && (newValue != null || newValue != "")){
+                    telefonpolje.getStyleClass().removeAll("poljeIspravno");
+                    telefonpolje.getStyleClass().add("poljeNeispravno");
+                    telefonValidan = false;
+                }
+                else {
+                    telefonpolje.getStyleClass().removeAll("poljeNeispravno");
+                    telefonpolje.getStyleClass().add("poljeIspravno");
+                    telefonValidan = true;
+                }
+            }
+        });
+    }
+    private String korektanUnos(){
+        String poruka = "";
+
+
+        if(!imeValidno)
+            poruka = poruka + "Netočno ime!\n";
+        if(!prezimeValidno)
+            poruka = poruka + "Netočno prezime!\n";
+        if(!brIndeksaValidan)
+            poruka = poruka + "Nekorektan broj indeksa\n";
+        if(!datumValidan || !jmbgValidan)
+            poruka = poruka + "Datum i JMBG se ne podudaraju\n";
+        if(!telefonValidan)
+            poruka = poruka + "Netočan broj telefona";
+        return poruka;
+    }
+
+    public void dugmeKliknuto(ActionEvent actionEvent) {
+        if(!korektanUnos().equals("")){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Nije validno");
+            alert.setHeaderText("Popunjeni formular nije validan");
+            alert.setContentText(korektanUnos());
+            alert.show();
+        }
+        else {
+            System.out.println("Ime: " + poljeIme.getText());
+            System.out.println("Prezime: " + poljePrezime.getText());
+            System.out.println("Broj indeksa" + poljeBrojIndeksa.getText());
+            System.out.println("Datum rođenja: " + datumRodjenja);
+            System.out.println("email: " + emailPolje.getText());
+            System.out.println("Adresa: " + AdresaPolje.getText());
+            System.out.println("Kontakt telefon: " + telefonpolje.getText());
+        }
+
+
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/formular.fxml"));
+        primaryStage.setTitle("Formular za studenta");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
